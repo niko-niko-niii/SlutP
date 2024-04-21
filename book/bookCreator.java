2c040af508210341ee0e485514d227dfc52c35de
vladyslav <vladyslav.slovak@elev.ga.ntig.se>
2024-04-21 22:34:56 +0200

    added bookCreator class

--- book/bookCreator.java
+++ book/bookCreator.java
@@ -1,31 +1,6 @@
-import java.util.Scanner;
-
 public class bookCreator {
-    static Scanner scanner = new Scanner(System.in);
-
-    public static void createBook() {
-        startloop:
-        do {
-            System.out.println("book name:");
-            String Bname = scanner.nextLine();
-            System.out.println("author name");
-            String AuthorName = scanner.nextLine();
-            System.out.println("Release date");
-            String releaseDate = scanner.nextLine();
-            //if function checks that releaseDate has only ints
-            if (releaseDate.equalsIgnoreCase(String.valueOf(Integer.parseInt(releaseDate)))) {
-                System.out.println("book created");
-                break startloop;
-            } else {
-                System.out.println("try again");
-            }
-
-        } while (true);
-    }
-
-
-    private static void addBook(String bookName, String authorName, String releaseDate){
-
-
+    public static void bbokc(){
+        
+        
     }
 }
