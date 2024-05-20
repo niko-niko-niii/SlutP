817d793fde05ebd71781b144ae07f82be55bfc3b
vladyslav.slovak <vladyslav.slovak@elev.ga.ntig.se>
2024-05-20 10:12:30 +0200

    librarian does not extend customer but user now

--- book/BookCreator.java
+++ book/BookCreator.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class BookCreator {
 
-    public  void bCreate(){
-        Scanner scanner =new Scanner(System.in);
+    public void bCreate() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("book name:");
         String name = scanner.nextLine();
         System.out.println("releaseDate(yyyymm):");
@@ -11,31 +11,32 @@ public class BookCreator {
         loop:
         do {
             //tar in bbara ints i datumet
-            try{
-                releaseDate= scanner.nextInt();
+            try {
+                releaseDate = scanner.nextInt();
                 //kollar så att datumet inte är för stor eller för liten
-                if(releaseDate<1000000 && releaseDate>-1000000){
-                break;
-                }else{
+                if (releaseDate < 1000000 && releaseDate > -1000000) {
+                    break;
+                } else {
                     System.out.println("date out of range");
                 }
 
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("write numbers only");
             }
 
-        }while(true);
+        } while (true);
         System.out.println("author");
-        String author= scanner.nextLine();
+        String author = scanner.nextLine();
         System.out.println("genre chose between:");
         System.out.println("romance, horror, sci fi, cook book, comics");
         String genre;
         // loopen kollar att genre är giltig
         loop:
-        while(true){
-            genre= scanner.nextLine();
-            switch (genre){
-                case "romance", "comics", "cook book", "sci fi", "horror": bCreator( name,releaseDate,author,genre);
+        while (true) {
+            genre = scanner.nextLine();
+            switch (genre) {
+                case "romance", "comics", "cook book", "sci fi", "horror":
+                    bCreator(name, releaseDate, author, genre);
                     break loop;
                 default:
                     System.out.println("genre not recogized try again");
@@ -46,13 +47,14 @@ public class BookCreator {
 
 
     }
+
     // bokfabrik
-    private void bCreator(String name, int releaseDate, String author, String genre){
-        Scanner scanner =new Scanner(System.in);
-        Book book=new Book(name,releaseDate,author,genre);
+    private void bCreator(String name, int releaseDate, String author, String genre) {
+        Scanner scanner = new Scanner(System.in);
+        Book book = new Book(name, releaseDate, author, genre);
         book.recieveBookText("text");
     }
-        
-        
-    }
+
+
+}
 
--- user/Librarian.java
+++ user/Librarian.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 
-public class Librarian extends Customer {
+public class Librarian extends User {
     //roll
-    private final String ROLE="l";
+    private final String ROLE = "l";
+
     public String getROLE() {
         return ROLE;
     }
- // Bibliotekarie creator. Den skapar bibliotekarier
+
+    // Bibliotekarie creator. Den skapar bibliotekarier
     Librarian(String name, String password) {
-        this.name=name;
-        this.password=password;
+        this.name = name;
+        this.password = password;
     }
+
     @Override
     public void passwordTest(String password) {
         super.passwordTest(password);
 
     }
+
     @Override
     protected void menu() {
         System.out.println(" borrow a book \n read a book \n return book \n log out \n see books");
@@ -24,29 +28,39 @@ public class Librarian extends Customer {
         while (true) {
             String answer = scanner.nextLine();
             switch (answer.toLowerCase()) {
-                case "borrow a book":addBook();
+                case "borrow a book":
+                    addBook();
                     break;
-                case "read a book": readBook();
+                case "read a book":
+                    readBook();
                     break;
-                case "return book":deleteBook();
+                case "return book":
+                    deleteBook();
                     break;
-                case "see books":sortedBooks();
-                break;
-                case "create book":createBook();
+                case "see books":
+                    sortedBooks();
                     break;
-                    case "delete book from libary":deleteLibaryBook();
+                case "create book":
+                    createBook();
                     break;
-                case "log out": break loop;
+                case "delete book from libary":
+                    deleteLibaryBook();
+                    break;
+                case "log out":
+                    break loop;
                 default:
                     System.out.println("check your spelling please");
+                    break;
             }
             System.out.println(" borrow a book \n read a book \n return book \n log out \n see books \n create book\n delete book from libary  ");
         }
     }
-    private void createBook(){ //låter bibliotekarien skapa en ny bok
+
+    private void createBook() { //låter bibliotekarien skapa en ny bok
         new BookCreator().bCreate();
     }
-    private void deleteLibaryBook(){ // låter radera en bok
+
+    private void deleteLibaryBook() { // låter radera en bok
         BookList.bookDelete();
     }
 }
